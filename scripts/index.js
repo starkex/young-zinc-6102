630471681a5d3addb0a06891cc269cf5cdf40486
starkex <starkanonimity511@gmail.com>
2022-08-24 20:12:52 +0530

    top-bar-complete

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+document.getElementById('canada').addEventListener('click',function(){
+    window.location.href= 'home.html'
+})
+document.getElementById('united').addEventListener('click',function(){
+    window.location.href= 'home.html'
+})
+document.getElementById('mexico').addEventListener('click',function(){
+    window.location.href= 'home.html'
+})

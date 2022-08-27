930cee7975c8db4dbe098f78467a988659c6f3cd
starkex <starkanonimity511@gmail.com>
2022-08-27 20:23:05 +0530

    local-storage-initial

---
+++ scripts/signup.js
@@ -0,0 +1,24 @@
+document.querySelector('form').addEventListener('submit',Signup)
+let LSData = JSON.parse(localStorage.getItem('Users')) || [];
+
+function Signup(event){
+    event.preventDefault();
+    console.log('billa')
+    let firstname = document.getElementById('first-name').value
+    let lastname = document.getElementById('last-name').value
+    let emailid = document.getElementById('email-id').value
+    let passwd = document.getElementById('passd').value
+    let conpasswd = document.getElementById('confirmpassd').value
+    let contact = document.getElementById('mobile').value
+    console.log(firstname)
+    let obj ={
+        'First name':firstname,
+        'Last name':lastname,
+        'Email':emailid,
+        'Password':passwd,
+        'Verifypass':conpasswd,
+        'MonileNo':contact
+    }
+    LSData.push(obj)
+    localStorage.setItem('Users',JSON.stringify(LSData))
+}
